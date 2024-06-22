6fa78280398399469edbf574eefa0cd8928dfac3
Soumyajyoti Biswas <soumyajyoti@outlook.com>
2024-06-22 15:24:55 +0530

    Adding a sample program_secrets.py file

---
+++ src/program_secrets.py
@@ -0,0 +1,36 @@
+"""
+This file contains the secrets for logging into 5paisa account.
+"""
+
+SECRETS = {
+    "Profile_Name_1": {
+        "CLIENT_CODE": "CLIENT_CODE_1",
+        "APP_NAME": "APP_NAME_1",
+        "APP_SOURCE": "APP_SOURCE_1",
+        "USER_ID": "USER_ID_1",
+        "PASSWORD": "PASSWORD_1",
+        "USER_KEY": "USER_KEY_1",
+        "ENCRYPTION_KEY": "ENCRYPTION_KEY_1",
+        "PIN": "PIN_1",
+    },
+    "Profile_Name_2": {
+        "CLIENT_CODE": "CLIENT_CODE_2",
+        "APP_NAME": "APP_NAME_2",
+        "APP_SOURCE": "APP_SOURCE_2",
+        "USER_ID": "USER_ID_2",
+        "PASSWORD": "PASSWORD_2",
+        "USER_KEY": "USER_KEY_2",
+        "ENCRYPTION_KEY": "ENCRYPTION_KEY_2",
+        "PIN": "PIN_2",
+    },
+    "Profile_Name_N": {
+        "CLIENT_CODE": "CLIENT_CODE_N",
+        "APP_NAME": "APP_NAME_N",
+        "APP_SOURCE": "APP_SOURCE_N",
+        "USER_ID": "USER_ID_N",
+        "PASSWORD": "PASSWORD_N",
+        "USER_KEY": "USER_KEY_N",
+        "ENCRYPTION_KEY": "ENCRYPTION_KEY_N",
+        "PIN": "PIN_N",
+    },
+}
